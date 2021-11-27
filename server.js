7954120d320378964ae614a713311ac6b69ae77b
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-11-27 14:39:16 -0800

    Connected to MySQL database

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'mySqlPassw0rd',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
