e238b65622110b291e8252104c1097bba69ac920
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-11-28 20:55:47 -0800

    Code cleanup

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'mySqlPassw0rd',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
